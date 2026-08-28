d3ce360fd9cbe8691169a4ad94405da20b8e1eb8
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-28 16:16:21 +0530

    chore: update eyexpert_app/lib/features/system_status/system_status_screen.dart

--- eyexpert_app/lib/features/system_status/system_status_screen.dart
+++ eyexpert_app/lib/features/system_status/system_status_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../core/constants/app_constants.dart';
 import '../../core/theme/app_colors.dart';
-import '../../core/utils/formatters.dart';
 import '../../shared/widgets/clinical_card.dart';
 import '../../shared/widgets/status_badge.dart';
 import '../../shared/widgets/model_provenance_card.dart';
