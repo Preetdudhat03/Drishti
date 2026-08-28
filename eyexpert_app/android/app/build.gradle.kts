2c0d37791799610e65cb8d46945359545a2f4c31
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-28 22:18:53 +0530

    chore: update eyexpert_app/android/app/build.gradle.kts

--- eyexpert_app/android/app/build.gradle.kts
+++ eyexpert_app/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.eyexpert.sih.eyexpert_app"
+    namespace = "com.drishti.sih.drishti_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.eyexpert.sih.eyexpert_app"
+        applicationId = "com.drishti.sih.drishti_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
