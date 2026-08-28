a211f3cc1e3ca7554fc2db719692c812ecef6350
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-28 23:00:55 +0530

    fix: modify 13 project files

--- build_docs.py
+++
@@ -1,7 +0,0 @@
-# Drishti SIH 2026 Master Documentation Builder
-import os, sys
-
-BASE_DIR = r'p:\pro\EyeXpert'
-DOCS_DIR = os.path.join(BASE_DIR, 'docs')
-os.makedirs(DOCS_DIR, exist_ok=True)
-print('DOCS_DIR ready:', DOCS_DIR)
--- test_here.py
+++
@@ -1,2 +0,0 @@
-﻿import os
-print("PowerShell Here-String Works Perfectly!")
