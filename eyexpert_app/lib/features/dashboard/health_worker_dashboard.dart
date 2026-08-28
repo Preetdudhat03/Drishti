3b104a501ffb3b478bb83b2177b993767e69bad4
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-29 00:57:49 +0530

    chore: update eyexpert_app/lib/features/dashboard/health_worker_dashboard.dart

--- eyexpert_app/lib/features/dashboard/health_worker_dashboard.dart
+++ eyexpert_app/lib/features/dashboard/health_worker_dashboard.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/theme/app_colors.dart';
-import '../../core/utils/formatters.dart';
 import '../../shared/widgets/status_badge.dart';
 import '../../shared/widgets/offline_status_bar.dart';
 import '../../shared/widgets/medical_disclaimer_banner.dart';
