f18899ba26a30da659936c7f9bb1e716ce7194b9
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-28 16:16:49 +0530

    test: add/update tests in eyexpert_app/test/widget/image_quality_screen_test.dart

--- eyexpert_app/test/widget/image_quality_screen_test.dart
+++ eyexpert_app/test/widget/image_quality_screen_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:eyexpert_app/features/quality/image_quality_screen.dart';
-import 'package:eyexpert_app/features/screening/screening_session_provider.dart';
 
 void main() {
   testWidgets('ImageQualityScreen renders quality evaluation interface', (WidgetTester tester) async {
