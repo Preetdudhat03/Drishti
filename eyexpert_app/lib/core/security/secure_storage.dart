cd76293c8084e0a982b20bd4d697939af3ad4d78
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-28 21:51:57 +0530

    chore: update eyexpert_app/lib/core/security/secure_storage.dart

--- eyexpert_app/lib/core/security/secure_storage.dart
+++ eyexpert_app/lib/core/security/secure_storage.dart
@@ -3,7 +3,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SecureStorage {
   static const String _keyToken = 'drishti_auth_token';
   static const String _keyUser = 'drishti_auth_user';
-  static const String _keyWorkflowMode = 'drishti_workflow_mode';
 
   static Future<void> saveToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
@@ -25,19 +24,8 @@ class SecureStorage {
     return prefs.getString(_keyUser);
   }
 
-  static Future<void> saveWorkflowMode(String mode) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(_keyWorkflowMode, mode);
-  }
-
-  static Future<String> getWorkflowMode() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString(_keyWorkflowMode) ?? 'DEMO';
-  }
-
   static Future<void> clearSession() async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.remove(_keyToken);
-    await prefs.remove(_keyUser);
+    await prefs.clear();
   }
 }
