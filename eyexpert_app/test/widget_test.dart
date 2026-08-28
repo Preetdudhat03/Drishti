5cd23fbb9c83fe7a00b1b4d863ea9a101013f7b4
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-28 17:01:43 +0530

    test: add/update tests in eyexpert_app/test/widget_test.dart

--- eyexpert_app/test/widget_test.dart
+++ eyexpert_app/test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:eyexpert_app/main.dart';
+import 'package:drishti_app/main.dart';
 
 void main() {
   testWidgets('Drishti App smoke test', (WidgetTester tester) async {
