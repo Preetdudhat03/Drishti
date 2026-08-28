c670cb6f6e5332756a52fcff3f5e25afced64a24
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-28 16:18:20 +0530

    chore: update eyexpert_app/lib/features/quality/image_quality_screen.dart

--- eyexpert_app/lib/features/quality/image_quality_screen.dart
+++ eyexpert_app/lib/features/quality/image_quality_screen.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../core/theme/app_colors.dart';
 import '../../core/utils/formatters.dart';
 import '../../shared/widgets/clinical_card.dart';
 import '../../shared/widgets/status_badge.dart';
