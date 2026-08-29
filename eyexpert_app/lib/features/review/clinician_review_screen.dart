225450de3030899b7bacf31d8390d9e82df5c68d
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-29 18:58:27 +0530

    chore: update eyexpert_app/lib/features/review/clinician_review_screen.dart

--- eyexpert_app/lib/features/review/clinician_review_screen.dart
+++ eyexpert_app/lib/features/review/clinician_review_screen.dart
@@ -14,6 +14,7 @@ import '../../shared/widgets/probability_bar.dart';
 import '../../shared/widgets/medical_disclaimer_banner.dart';
 import '../auth/auth_provider.dart';
 import 'review_queue_provider.dart';
+import '../../shared/widgets/connection_status_pill.dart';
 
 class ClinicianReviewScreen extends ConsumerStatefulWidget {
   final ScreeningCaseModel screeningCase;
