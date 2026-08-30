4518808d3a19bc6a4b86cebead8ce183a33b847a
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-30 12:36:01 +0530

    chore: update eyexpert_app/lib/main_phc.dart

--- eyexpert_app/lib/main_phc.dart
+++ eyexpert_app/lib/main_phc.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/theme/app_theme.dart';
-import 'core/constants/app_constants.dart';
-import 'data/models/user_model.dart';
 import 'data/models/screening_case_model.dart';
 import 'data/services/supabase_service.dart';
 import 'shared/widgets/responsive_scaffold.dart';
