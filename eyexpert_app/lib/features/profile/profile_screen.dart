8d66b81a250fab8d1f3347f8ec4da385eb3dd4c8
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-30 12:37:36 +0530

    chore: update eyexpert_app/lib/features/profile/profile_screen.dart

--- eyexpert_app/lib/features/profile/profile_screen.dart
+++ eyexpert_app/lib/features/profile/profile_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/theme/app_colors.dart';
-import '../../data/models/user_model.dart';
 import '../../shared/widgets/clinical_card.dart';
 import '../../shared/widgets/primary_button.dart';
 import '../../shared/widgets/medical_disclaimer_banner.dart';
