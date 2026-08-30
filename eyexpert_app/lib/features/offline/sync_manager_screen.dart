e2dc68256f0bb91c2257e4e46cf5949fb8d971c4
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-08-30 12:38:37 +0530

    chore: update eyexpert_app/lib/features/offline/sync_manager_screen.dart

--- eyexpert_app/lib/features/offline/sync_manager_screen.dart
+++ eyexpert_app/lib/features/offline/sync_manager_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../core/constants/app_constants.dart';
 import '../../core/theme/app_colors.dart';
 import '../../core/utils/formatters.dart';
 import '../../shared/widgets/clinical_card.dart';
