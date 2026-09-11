53be311110c2b9e9ce1bf62496e197de5d3da631
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-09-11 21:54:55 +0530

    chore: update eyexpert_app/lib/features/screening/patient_intake_screen.dart

--- eyexpert_app/lib/features/screening/patient_intake_screen.dart
+++ eyexpert_app/lib/features/screening/patient_intake_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/theme/app_colors.dart';
+import '../../core/utils/responsive_layout.dart';
 import '../../shared/widgets/clinical_card.dart';
 import '../../shared/widgets/status_badge.dart';
 import '../../shared/widgets/primary_button.dart';
