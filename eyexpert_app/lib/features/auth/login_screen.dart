ee5c8ed555276c03846fd7aeb257918e17a5e0f3
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-09-11 21:32:57 +0530

    chore: update eyexpert_app/lib/features/auth/login_screen.dart

--- eyexpert_app/lib/features/auth/login_screen.dart
+++ eyexpert_app/lib/features/auth/login_screen.dart
@@ -267,7 +267,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
               controller: _emailController,
               style: const TextStyle(color: AppColors.textPrimary, fontSize: 14),
               decoration: InputDecoration(
-                hintText: 'healthworker@dridshti.org or ophthalmologist@drishti.org',
+                hintText: 'healthworker@drishti.org or ophthalmologist@drishti.org',
                 hintStyle: const TextStyle(color: AppColors.textMuted, fontSize: 13),
                 prefixIcon: const Icon(Icons.email_outlined, color: AppColors.textSecondary, size: 18),
                 fillColor: Colors.white,
