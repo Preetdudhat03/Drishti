0b4d949014d2953ca9d2bb02e4d6887672a5a5a7
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-09-11 21:32:46 +0530

    chore: update eyexpert_app/lib/features/auth/login_screen.dart

--- eyexpert_app/lib/features/auth/login_screen.dart
+++ eyexpert_app/lib/features/auth/login_screen.dart
@@ -23,8 +23,8 @@ class LoginScreen extends ConsumerStatefulWidget {
 
 class _LoginScreenState extends ConsumerState<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
-  final _emailController = TextEditingController(text:'Ophthalmologist@drishti.org');
-  final _passwordController = TextEditingController(text:'RetinaSpecialist@drishti.org');
+  final _emailController = TextEditingController(text: 'ophthalmologist@drishti.org');
+  final _passwordController = TextEditingController(text: 'RetinaSpecialist@2026!');
   bool _obscurePassword = true;
   UserRole _selectedRole = UserRole.clinician;
 
