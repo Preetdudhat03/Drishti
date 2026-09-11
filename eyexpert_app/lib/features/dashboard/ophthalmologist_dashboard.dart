f57a84700c26fc19ecdf5d327817c36f33941c40
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-09-11 22:18:09 +0530

    chore: update eyexpert_app/lib/features/dashboard/ophthalmologist_dashboard.dart

--- eyexpert_app/lib/features/dashboard/ophthalmologist_dashboard.dart
+++ eyexpert_app/lib/features/dashboard/ophthalmologist_dashboard.dart
@@ -99,7 +99,7 @@ class OphthalmologistDashboard extends ConsumerWidget {
                           Text(
                             '$doctorName!',
                             style: const TextStyle(
-                              fontSize: 34,
+                              fontSize: 40,
                               fontWeight: FontWeight.w900,
                               color: AppColors.primary,
                               letterSpacing: -0.8,
